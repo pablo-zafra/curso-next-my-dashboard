8490657af673ef19c3cf964deba308e1d7820a34
pablozafra90 <pablozafra90@hotmail.com>
2025-02-07 19:11:19 +0100

    43-wip

--- src/app/dashboard/counter/page.tsx
+++ src/app/dashboard/counter/page.tsx
@@ -1,8 +1,28 @@
+"use client";
+
+import { useState } from "react";
 
 export default function CounterPage() {
+  const [count, setCount] = useState(10);
+
   return (
-    <div>
-      <h1>Counter Page</h1>
+    <div className="flex flex-col items-center justify-center w-full h-full">
+      <span>Productos en el carrito de compra</span>
+      <span className="text-9xl">{count}</span>
+      <div className="flex items-center justify-center mt-4">
+        <button
+          className="flex items-center justify-center p-2 rounded-xl bg-gray-900 text-white hover:bg-gray-600 transition-all w-[100px] mr-2"
+          onClick={() => setCount(count + 1)}
+        >
+          +1
+        </button>
+        <button
+          className="flex items-center justify-center p-2 rounded-xl bg-gray-900 text-white hover:bg-gray-600 transition-all w-[100px] mr-2"
+          onClick={() => setCount(count - 1)}
+        >
+          -1
+        </button>
+      </div>
     </div>
   );
 }
