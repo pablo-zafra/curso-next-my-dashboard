7b6c85bd58b10f75bc9cc9850ff87328410fe0c3
Pablo Zafra <hola@pablozafra.es>
2025-02-11 19:33:09 +0100

    56-done

--- src/app/dashboard/pokemons/page.tsx
+++ src/app/dashboard/pokemons/page.tsx
@@ -11,6 +11,7 @@ const getPokemons = async (limit = 20, offset = 0): Promise<SimplePokemon[]> => 
     name: pokemon.name,
   }));
 
+
   return pokemons;
 };
 
@@ -20,20 +21,6 @@ export default async function PokemonPage() {
   return (
     <div className="flex flex-col items-center justify-center w-full h-full">
       <h2 className="">Pokemons</h2>
-      {/* <div className="grid grid-cols-4 gap-4 mt-4">
-        {pokemons.map(pokemon => (
-          <div key={pokemon.id} className="flex flex-col items-center">
-            <Image
-              key={pokemon.id}
-              src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/${pokemon.id}.svg`}
-              width={100}
-              height={100}
-              alt={pokemon.name}
-            />
-            <h4>{pokemon.name}</h4>
-          </div>
-        ))}
-      </div> */}
       <PokemonGrid pokemons={pokemons} />
     </div>
   );
--- src/app/pokemons/components/PokemonGrid.tsx
+++ src/app/pokemons/components/PokemonGrid.tsx
@@ -8,23 +8,9 @@ interface Props {
 
 export const PokemonGrid = ({ pokemons }: Props) => {
   return (
-    // <div className="grid grid-cols-4 gap-4 mt-4">
-    //   {pokemons.map(pokemon => (
-    //     <div key={pokemon.id} className="flex flex-col items-center">
-    //       <Image
-    //         key={pokemon.id}
-    //         src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/${pokemon.id}.svg`}
-    //         width={100}
-    //         height={100}
-    //         alt={pokemon.name}
-    //       />
-    //       <h4>{pokemon.name}</h4>
-    //     </div>
-    //   ))}
-    // </div>
-    <div className="grid grid-cols-4 gap-4 mt-4">
+    <div className="grid grid-cols-2 gap-4 mt-4">
       {pokemons.map(pokemon => (
-        <PokemonCard key={pokemon.id} />
+        <PokemonCard key={pokemon.id} pokemon={pokemon} />
       ))}
     </div>
   );
