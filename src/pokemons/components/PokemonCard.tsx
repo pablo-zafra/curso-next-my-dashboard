cb36524a5c475a803f018e3fe2bcc402740effd3
Pablo Zafra <hola@pablozafra.es>
2025-02-20 12:11:44 +0100

    95-done

--- src/pokemons/components/PokemonCard.tsx
+++ src/pokemons/components/PokemonCard.tsx
@@ -1,8 +1,11 @@
+'use client';
+
 import Link from 'next/link';
 import Image from 'next/image';
 import { SimplePokemon } from '../interfaces/simple-pokemon';
-import { IoHeartOutline } from 'react-icons/io5';
-import { useAppSelector } from '../../store/index';
+import { IoHeart, IoHeartOutline } from 'react-icons/io5';
+import { useAppDispatch, useAppSelector } from '../../store/index';
+import { toogleFavorite } from '@/store/pokemons/pokemonsSlice';
 
 interface Props {
   pokemon: SimplePokemon;
@@ -10,7 +13,12 @@ interface Props {
 
 export const PokemonCard = ({ pokemon }: Props) => {
   const pokemonState = useAppSelector(state => state.pokemon);
-  const isFavorite = pokemonState[pokemon.id] ? true : false;
+  const dispatch = useAppDispatch();
+  const isFavorite = pokemonState[pokemon.id.toString()] ? true : false;
+  const onToogle = () => {
+    dispatch(toogleFavorite(pokemon));
+  };
+
   return (
     <div className="mx-auto right-0 mt-2">
       <div className="flex flex-col bg-white rounded overflow-hidden shadow-lg">
@@ -36,20 +44,20 @@ export const PokemonCard = ({ pokemon }: Props) => {
           </div>
         </div>
         <div className="border-b">
-          <Link
-            href="/dashboard/pokemons"
+          <button
+            onClick={onToogle}
             className="items-center px-4 py-2 hover:bg-gray-100 flex"
           >
             <div className="text-red-600">
-              <IoHeartOutline />
+              {isFavorite ? <IoHeart /> : <IoHeartOutline />}
             </div>
-            <div className="pl-3">
+            <div className="pl-3 p-3">
               <p className="text-sm font-medium text-gray-800 leading-none">
                 {isFavorite ? 'Es favorito' : 'No es favorito'}
               </p>
-              <p className="text-xs text-gray-500">View your campaigns</p>
+              {/* <p className="text-xs text-gray-500">View your campaigns</p> */}
             </div>
-          </Link>
+          </button>
         </div>
       </div>
     </div>
