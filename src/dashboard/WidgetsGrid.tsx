77e90c5fac06173bdccb75b6a06c880a302500ac
Pablo Zafra <hola@pablozafra.es>
2025-04-11 09:16:05 +0200

    tailwind-and-prettier-ready

--- src/dashboard/WidgetsGrid.tsx
+++ src/dashboard/WidgetsGrid.tsx
@@ -1,23 +1,22 @@
 'use client';
 
-import { IoCafeOutline } from "react-icons/io5"
-import { SimpleWidget } from "./SimpleWidget"
-import { useAppSelector } from "@/store"
+import { IoCafeOutline } from 'react-icons/io5';
+import { SimpleWidget } from './SimpleWidget';
+import { useAppSelector } from '@/store';
 
 export const WidgetsGrid = () => {
+  const cartState = useAppSelector(state => state.counter.count);
 
-    const cartState = useAppSelector(state => state.counter.count)
-
-    return (
-        <div className="flex flex-col items-center p-2 w-full gap-4">
-            <SimpleWidget
-                title="Contador"
-                subtitle="Client counter"
-                label={cartState.toString()}
-                icon={<IoCafeOutline size={50} className="text-blue-500" />}
-                href="./counter"
-            />
-            <SimpleWidget />
-        </div>
-    )
-}
+  return (
+    <div className="flex flex-col items-center p-2 w-full gap-4">
+      <SimpleWidget
+        title="Contador"
+        subtitle="Client counter"
+        label={cartState.toString()}
+        icon={<IoCafeOutline size={50} className="text-blue-500" />}
+        href="./counter"
+      />
+      <SimpleWidget />
+    </div>
+  );
+};
